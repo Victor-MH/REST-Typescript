927ba21cde3db8d624517dede4da34c01f253f3b
VicoLap <victormu497@gmail.com>
2021-02-20 20:40:41 -0600

    Variables de entorno

--- src/database.ts
+++ src/database.ts
@@ -1,4 +1,5 @@
 import mongoose, { ConnectionOptions } from 'mongoose';
+import config from './config';
 
 (async () => {
     try {
@@ -6,9 +7,11 @@ import mongoose, { ConnectionOptions } from 'mongoose';
         const mongooseOptions: ConnectionOptions = {
             useUnifiedTopology: true,
             useNewUrlParser: true,
+            // user: config.MONGO_USER,
+            // pass: config.MONGO_PASSWORD,
         }
         
-        const db = await mongoose.connect('mongodb://localhost/mern-database',
+        const db = await mongoose.connect(`mongodb://${config.MONGO_HOST}/${config.MONGO_DATABASE}`,
         mongooseOptions);
         console.log('database is connected to ', db.connection.name);
     } catch (error) {
